54ccdfb49d9fcc290ac16bf330956fff3702459b
th3m7J0 <a.mecheref@esi-sba.dz>
2020-07-13 23:05:18 +0100

    add get paramters

--- crud.js
+++ crud.js
@@ -13,8 +13,11 @@ module.exports = {
     },
     get: (resourceModel, type, filter, middleware)=> {
         return catchAsync(async (req,res,next)=>{
+            let count = 0;
+            let sortObject = {};
             let projection = {};
-            let {display,expand} = req.query;
+            let result = {};
+            let {display,expand,limit,sort,page} = req.query;
             // 1. support projection in order to get the desired fields
             if (display){
                 let elements = display.split(',');
@@ -25,7 +28,30 @@ module.exports = {
 
             let resource = projection?  resourceModel[type](filter(req),projection):
                 resourceModel[type](filter(req));
-            // 2. support populate in order to get all the data
+
+            if(type === 'find') {
+
+                // 2. support sort parameter
+                if (sort) {
+                    let elements = sort.split(',');
+                    elements.forEach(element => {
+                        sortObject[element] = 1;
+                    })
+                    resource.sort(sortObject);
+                }
+                // 3. support limit parameter
+                if (limit) {
+                    resource.limit(parseInt(limit));
+                }
+                // 4. support pagination
+                if (page) {
+                    resource.skip((parseInt(page) - 1) * parseInt(limit));
+                    let c = await resourceModel.countDocuments(filter(req));
+                    count = parseInt(c) % parseInt(limit) === 0 ? Math.trunc(parseInt(c)/parseInt(limit)) : Math.trunc(parseInt(c)/parseInt(limit))+1;
+                }
+            }
+
+            // 5. support populate in order to get all the data
             if(expand){
                 let elements = expand.split(',');
                 elements.forEach(element=>{
@@ -33,14 +59,17 @@ module.exports = {
                 })
             }
             // final result
-            resource = await resource;
+            if(type === 'find' && page)
+                result = {resource:await resource,count: count};
+            else
+                result = await resource;
 
             if(type ==='findOne' && !resource)
                 return next(new AppError(404,'resource not found'));
 
             if(middleware)
                 return next();
-            res.status(200).json(resource);
+            res.status(200).json(result);
         },'crud => get');
     },
     update: (resourceModel,type, filter, data, middleware)=>{
